f8bc375ab673130dce6a09fed444637da347aac4
Shub3am <shubhamvishwakarma0604@gmail.com>
2023-12-10 23:56:10 +0530

    FEATURE: Working on Backend for adding feature

--- backend/seeder.js
+++ backend/seeder.js
@@ -4,38 +4,43 @@ const moment = require("moment")
 
 
 function Seeder(csvFile, Table, db) {
-db.run(`CREATE TABLE IF NOT EXISTS ${Table}(ID INTEGER PRIMARY KEY AUTOINCREMENT, Name TEXT NOT NULL, Description TEXT ,Founded TEXT NOT NULL, Industry TEXT NOT NULL ON CONFLICT REPLACE DEFAULT 'Unknown', Funding_Amount INTEGER NOT NULL ON CONFLICT REPLACE DEFAULT 'Unknown', Investor TEXT NOT NULL ON CONFLICT REPLACE DEFAULT 'Unknown', Investment_Type TEXT NOT NULL ON CONFLICT REPLACE DEFAULT 'Unknown', City TEXT NOT NULL ON CONFLICT REPLACE DEFAULT 'Unknown' )`, (result)=> {
-    const parsedCSV = fs.createReadStream(csvFile).pipe(csvParser())
+db.run(`CREATE TABLE IF NOT EXISTS ${Table} (ID INTEGER PRIMARY KEY AUTOINCREMENT, Name TEXT NOT NULL, Description TEXT ,Founded TEXT NOT NULL, Industry TEXT NOT NULL ON CONFLICT REPLACE DEFAULT 'Unknown', Funding_Amount INTEGER NOT NULL ON CONFLICT REPLACE DEFAULT 'Unknown', Investor TEXT NOT NULL ON CONFLICT REPLACE DEFAULT 'Unknown', Investment_Type TEXT NOT NULL ON CONFLICT REPLACE DEFAULT 'Unknown', City TEXT NOT NULL ON CONFLICT REPLACE DEFAULT 'Unknown' )`)
+db.all("SELECT * from STARTUP_DIRECTORY", (err, result)=> {
+    if (!result.length && !err) {
+        const parsedCSV = fs.createReadStream(csvFile).pipe(csvParser())
 
-parsedCSV.on("data", (data)=> {
-    /* SCHEMA 
-    SNo: '',
-  Date: ,
-  StartupName: ,
-  IndustryVertical: '',
-  SubVertical: '',
-  CityLocation: '',
-  InvestorsName: ,
-  InvestmentType: ,
-  AmountInUSD: ,
-  Remarks: '' 
-  */
-
-    let {StartupName, IndustryVertical, SubVertical, CityLocation,   InvestorsName ,
-        InvestmentType,
-        AmountInUSD} = data
-    let date = moment(data.Date, 'DD/MM/YYYY')
-    date = date.format('YYYY-MM-DD')
-    let dataValues = { $name: StartupName, $description: SubVertical, $founded: date, $industry: IndustryVertical, $funding: AmountInUSD,$investor: InvestorsName,$investmentType: InvestmentType , $city: CityLocation}
-        Object.keys(dataValues).forEach(property=> {
-            if (!dataValues[property]) {
-                dataValues[property] = null
-            }
+        parsedCSV.on("data", (data)=> {
+            /* SCHEMA 
+            SNo: '',
+          Date: ,
+          StartupName: ,
+          IndustryVertical: '',
+          SubVertical: '',
+          CityLocation: '',
+          InvestorsName: ,
+          InvestmentType: ,
+          AmountInUSD: ,
+          Remarks: '' 
+          */
+        
+            let {StartupName, IndustryVertical, SubVertical, CityLocation,   InvestorsName ,
+                InvestmentType,
+                AmountInUSD} = data
+            let date = moment(data.Date, 'DD/MM/YYYY')
+            date = date.format('YYYY-MM-DD')
+            let dataValues = { $name: StartupName, $description: SubVertical, $founded: date, $industry: IndustryVertical, $funding: AmountInUSD,$investor: InvestorsName,$investmentType: InvestmentType , $city: CityLocation}
+                Object.keys(dataValues).forEach(property=> {
+                    if (!dataValues[property]) {
+                        dataValues[property] = null
+                    }
+                })
+            db.run(`INSERT INTO ${Table} VALUES(?, $name, $description, $founded, $industry, $funding, $investor, $investmentType, $city)`, dataValues)
         })
-    db.run(`INSERT INTO ${Table} VALUES(?, $name, $description, $founded, $industry, $funding, $investor, $investmentType, $city)`, dataValues)
-})
-})
 
+    } else {
+        console.error(err)
+    }
+})
 }
 
 module.exports = Seeder
--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,9 @@ const express = require("express")
 const app = express()
 const sqlite3 = require('sqlite3').verbose();
 const Seeder = require( "./seeder");
+const cors = require("cors")
 
+app.use(cors())
 app.use(express.json());
 Table = "STARTUP_DIRECTORY"
 const db = new sqlite3.Database("./database/directory.db")
@@ -12,16 +14,44 @@ db.on("error", (err) => console.error(`Error Caused Due ${err}`)) // Throwing Er
 //Check Whether Database Exists, If not, seeds the Data
 Seeder("./startup_funding.csv", Table, db) //Parameters: (CSV FILE, TABLE NAME, DB Connection)
 
-app.get("/", (req,res)=> {
-    db.all(`select * from ${Table}`, (err,rows)=> {
+app.post("/", (req,res)=> {
+    let pageNo = req.body.page
+    query = `select * from ${Table}`
+
+    //Returning Only 10 Records Per Page for SSR
+    if(pageNo && pageNo > 1) {
+        query += ` where id >= ${pageNo}0 and id <= ${pageNo+1}0 `
+    } else if (pageNo && pageNo == 1) {
+        query += ' where id >= 0 and id <= 10'
+    }
+    console.log(query)
+    db.all(query, (err,rows)=> {
         err ? res.status(404).json(`Error Occured Due to ${err}`) : res.json(rows)
-    })
+    }) 
 })
 
+app.post("/addNew", (req,res)=> {
+    req.body.startup ? null : res.status(404).json("Data Missing")
+    const startupData = req.body.startup
+    startupData.funding = Number(startupData.funding)
+    startupData['investmentType'] = "?"
+    //schema of startupData: {name, description, funding, industry, city, date}
+    Object.keys(startupData).forEach( item=> {
+        (startupData[item] ? null : startupData[item] = '')
+        startupData[`$${item}`] = startupData[item]
+        delete startupData[item]
+    })
+    console.log(startupData, 'sdae')
+    db.run(`INSERT INTO ${Table} VALUES(?, $name, $description, $founded, $industry, $funding, $investor, $investmentType, $city)`, startupData, (err, result)=> {
+        console.log(err,result)
+        (err ? res.json(err) : res.json(result))
+    })
 
 
 
 
+    // return res.json(true) | false
+})
 
 
 
