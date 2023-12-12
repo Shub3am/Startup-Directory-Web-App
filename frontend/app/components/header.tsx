74e9bd03ed83ff752998e14e449ae969da1e2cea
Shub3am <shubhamvishwakarma0604@gmail.com>
2023-12-12 23:13:57 +0530

    STYLE: Responsive Header

--- frontend/app/components/header.tsx
+++ frontend/app/components/header.tsx
@@ -2,32 +2,27 @@ import Search from "./Search/page"
 import AddNew from "../addNew/page"
 export default async function HEADER() {
     return (<header className="bg-gray-50">
-    <div className="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 lg:px-8">
-      <div className="flex items-center justify-end gap-4">
-        <div className="flex items-center gap-4">
-<Search/>
+    <div className="mx-auto flex flex-col align-items-center md:flex-row justify-center md:justify-between max-w-screen-xl px-4 py-8 sm:px-6 lg:px-8">
+
+        
   
-          <a
-            href="#"
-            className="block shrink-0 rounded-full bg-white p-2.5 text-gray-600 shadow-sm hover:text-gray-700"
-          >
-  
-          </a>
-        </div>
-  
-        <span aria-hidden="true" className="block h-6 w-px rounded-full bg-gray-200"></span>
+        
   
   
-      </div>
-  
-      <div className="mt-8">
+
+      <div className="m-auto flex justify-center flex-col">
         <h1 className="text-2xl font-bold text-gray-900 sm:text-3xl">Welcome to Sperch!</h1>
   
-        <p className="mt-1.5 text-sm text-gray-500">
+        <p className="mt-1.5 text-sm my-4 text-gray-500">
           Find Startups that Rock. Keep it up! 🚀
         </p>
         <AddNew/>
       </div>
+      <div className="flex my-4 mx-auto flex-col justify-center align-items-center m-auto ">
+
+<Search/>
+  
+        </div>
     </div>
   </header>)
 }
