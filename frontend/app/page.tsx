8159afd05b8e52047e2e0b13fb00a25a02839478
Shub3am <shubhamvishwakarma0604@gmail.com>
2023-12-13 00:26:06 +0530

    IMPORTANT: Modified Next Config for Deployment

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,4 +1,3 @@
-
 import Card from "./components/card/card"
 import AddNew from "./addNew/page"
 
@@ -7,15 +6,14 @@ type Schema = {
 }
 
 async function getRecords() {
-  let res = await fetch(process.env.SERVER_URL, {method: "POST", headers: {"Content-Type": "application/json"}, cache: "no-store" , body: JSON.stringify({page: 1})})
+  let res = await fetch(process.env.SERVER_URL, {method: "POST", headers: {"Content-Type": "application/json"}, body: JSON.stringify({page: 1})})
   let data = await res.json()
   return data
 }
 
-export default async function Home({searchParams}) {
-  const data = await getRecords()
-
-  let Startup_Card = data.map( (single_startup: Schema) => {
+export default async function Home({searchParams}: {searchParams: {search: string | undefined}}) {
+  let data:[Schema] = await getRecords()
+  const Startup_Card = data.map( (single_startup: Schema) => { 
     let {ID, Name, Founded, City, Funding_Amount, Description, Industry} = single_startup
     
     let query = <div key={ID}><Card name={Name} foundingYear={Founded} Description={Description} Industry={Industry} City={City} Funding={Funding_Amount} /></div>
