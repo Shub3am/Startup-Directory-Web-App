0c1cde738e278849bbe47ef49a2e9d9a52557ad6
Shub3am <shubhamvishwakarma0604@gmail.com>
2023-12-19 13:59:56 +0530

    FIX: Fixed query showing page=0

--- frontend/app/components/pagination/pagination.tsx
+++ frontend/app/components/pagination/pagination.tsx
@@ -13,9 +13,9 @@ export default function Pagination() {
     <span className="absolute top-0 left-0 mt-1 ml-1 h-full w-full rounded bg-gray-700"></span>
     <span className="fold-bold relative inline-block h-full w-full rounded border-2 border-black bg-black px-3 py-1 text-base font-bold text-white transition duration-100 hover:bg-gray-900 hover:text-yellow-500 dark:bg-black">Home</span>
 </button><button onClick={()=> {
-        if (pageNo && pageNo > 0) {
+        if (pageNo && pageNo > 1) {
             Router.push(`?page=${Number(pageNo)-1}`)
-        }
+        } else { Router.push('/')}
     }} className="relative mx-2">
     <span className="absolute top-0 left-0 mt-1 ml-1 h-full w-full rounded bg-gray-700"></span>
     <span className="fold-bold relative inline-block h-full w-full rounded border-2 border-black bg-black px-3 py-1 text-base font-bold text-white transition duration-100 hover:bg-gray-900 hover:text-yellow-500 dark:bg-black">Prev</span>
